9bc1415c246d01640ba17481dc503e548fb30823
Richard Capraro <richard.capraro@gmail.com>
2021-10-13 22:52:54 +0200

    more exercises on Threads

--- src/main/scala/lectures/part3concurrency/ThreadCommunication.scala
+++ src/main/scala/lectures/part3concurrency/ThreadCommunication.scala
@@ -1,5 +1,8 @@
 package lectures.part3concurrency
 
+import scala.collection.mutable
+import scala.util.Random
+
 object ThreadCommunication {
 
   class SimpleContainer {
@@ -77,8 +80,133 @@ object ThreadCommunication {
       producer.start()
     }
 
-    smartProdCons()
+    // smartProdCons()
+
+    /*
+    buffer: many values
+    producer -> [ ? ? ? ? ] -> consumer
+    */
+
+    def prodConsLargeBuffer(): Unit = {
+      val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
+      val capacity = 3
+
+      val consumer = new Thread(() => {
+        val random = new Random()
+        while (true) {
+          buffer.synchronized {
+            if (buffer.isEmpty) {
+              println("[consumer] buffer empty, waiting...")
+              buffer.wait()
+            }
+
+            // there must be at least ONE value in the buffer
+            val x = buffer.dequeue()
+            println("[consumer] consumed " + x)
+
+            // hey producer, are you lazy ?
+            buffer.notify()
+          }
+
+          Thread.sleep(random.nextInt(500)) //simulates long computing from the consumer
+        }
+      })
+
+      val producer = new Thread(() => {
+        val random = new Random()
+        var i = 0
+
+        while (true) {
+          buffer.synchronized {
+            if (buffer.size == capacity) {
+              println("[producer] buffer is full, waiting...")
+              buffer.wait()
+            }
+
+            // there must be at least ONE EMPTY SPACE in the buffer
+            println("[producer] producing " + i)
+            buffer.enqueue(i)
+
+            // hey consumer, new food for you !
+            buffer.notify()
+
+            i += 1
+          }
+
+          Thread.sleep(random.nextInt(500))
+        }
+      })
+
+      consumer.start()
+      producer.start()
+    }
+
+    // prodConsLargeBuffer()
+
+    /*
+    producer1 ->  [ ? ? ? ? ] -> consumer1
+    producer2 ->  [ ? ? ? ? ] -> consumer2
+    */
+
+    class Consumer(id: Int, buffer: mutable.Queue[Int]) extends Thread {
+      override def run(): Unit = {
+        val random = new Random()
+        while (true) {
+          buffer.synchronized {
+            while (buffer.isEmpty) {  //handle the case where multiple consumers are notified !
+              println(s"[consumer $id] buffer empty, waiting...")
+              buffer.wait()
+            }
+
+            // there must be at least ONE value in the buffer
+            val x = buffer.dequeue()
+            println(s"[consumer $id] consumed " + x)
+
+            // notify somebody - cons or prod
+            buffer.notify()
+          }
+
+          Thread.sleep(random.nextInt(500))
+        }
+      }
+    }
+
+    class Producer(id: Int, buffer: mutable.Queue[Int], capacity: Int) extends Thread {
+      override def run(): Unit = {
+        val random = new Random()
+        var i = 0
+
+        while (true) {
+          buffer.synchronized {
+            while (buffer.size == capacity) {
+              println(s"[producer $id] buffer is full, waiting...")
+              buffer.wait()
+            }
+
+            // there must be at least ONE EMPTY SPACE in the buffer
+            println(s"[producer $id] producing " + i)
+            buffer.enqueue(i)
+
+            // notify somebody - cons or prod
+            buffer.notify()
+
+            i += 1
+          }
+
+          Thread.sleep(random.nextInt(500))
+        }
+      }
+    }
+
+    def multiProdCons(nConsumers: Int, nProducers: Int): Unit = {
+      val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
+      val capacity = 20
+
+      (1 to nConsumers).foreach(i => new Consumer(i, buffer).start())
+      (1 to nProducers).foreach(i => new Producer(i, buffer, capacity).start())
+    }
+
+    multiProdCons(3, 3)
 
   }
-
 }
